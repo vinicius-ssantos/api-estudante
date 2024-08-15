5172b4ca9f9cc02256ed22064f1535280036bc8d
Vinicius <viniciusolicveira@gmail.com>
2024-08-14 23:22:00 -0300

    chore(docker): add Docker and Docker Compose setup

---
+++ src/main/java/com/viniciussantos/EstudantesApiApplication.java
@@ -0,0 +1,13 @@
+package com.viniciussantos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudantesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstudantesApiApplication.class, args);
+    }
+
+}
